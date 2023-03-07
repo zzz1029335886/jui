d5d3fd906010b47e63cfa9e8f83d04a90516684b
-zerry- <zzz1029335886@qq.com>
2023-03-07 10:55:25 +0800

    modal_bottom_sheet 移到src内

--- lib/src/common/jui_bottom_sheet.dart
+++ lib/src/common/jui_bottom_sheet.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:jui/jui.dart';
-import 'package:jui_modal_bottom_sheet/modal_bottom_sheet.dart';
+import 'package:jui/src/modal_bottom_sheet/lib/modal_bottom_sheet.dart';
 
 class JUIBottomSheetModalsPageRoute extends MaterialWithModalsPageRoute {
   JUIBottomSheetModalsPageRoute(
