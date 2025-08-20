1867c92569345dc31b4f6aeeb0f133d09bda166a
zhangzezhong <zzz@1725250849092365.onaliyun.com>
2025-08-20 22:45:12 +0800

    删除image_pickers

--- lib/jui.dart
+++ lib/jui.dart
@@ -5,5 +5,5 @@ export 'src/other/cf_text_field.dart';
 export 'src/other/cf_picture_selection.dart';
 export 'src/jui_setting.dart';
 
-export 'package:image_pickers/image_pickers.dart';
+// export 'package:image_pickers/image_pickers.dart';
 export 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
